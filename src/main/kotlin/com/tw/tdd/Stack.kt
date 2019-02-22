7e2e517e0e5ec0543b67f1445f918583361f09a8
Gaspar Soto <gsoto@thoughtworks.com>
2019-02-22 16:06:12 -0300

    dev-day

--- src/main/kotlin/com/tw/tdd/Stack.kt
+++ src/main/kotlin/com/tw/tdd/Stack.kt
@@ -1,5 +1,32 @@
 package com.tw.tdd
 
 class Stack {
+    private var elements = IntArray(3)
+    var cont = 0
 
+    fun push(element: Int) {
+        if (isFull()) {
+            duplicateCapacity()
+        }
+        elements[cont++] = element
+
+    }
+
+    private fun duplicateCapacity() {
+        val myElements = IntArray(cont * 2)
+        for ((index, data) in elements.withIndex()) {
+            myElements[index] = data
+        }
+        elements = myElements
+    }
+
+    private fun isFull() = elements.size == cont
+
+    fun size(): Int {
+        return cont
+    }
+
+    fun pop(): Int {
+        return elements[--cont]
+    }
 }
--- src/test/kotlin/com/tw/tdd/StackTest.kt
+++ src/test/kotlin/com/tw/tdd/StackTest.kt
@@ -1,12 +1,60 @@
 package com.tw.tdd
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class StackTest {
+    lateinit var stack : Stack
+
+    @BeforeEach
+    fun setUp() {
+        stack = Stack()
+    }
 
     @Test
-    fun firstTest() {
-        assertEquals(1,1)
+    fun pushOneElement() {
+        stack.push(6)
+
+        assertEquals(stack.size(), 1)
     }
+
+    @Test
+    fun pushTwoElement() {
+        stack.push(6)
+        stack.push(6)
+
+        assertEquals(stack.size(), 2)
+    }
+
+    @Test
+    fun pushesAndPopOneElement() {
+        stack.push(6)
+
+        val six = stack.pop()
+        assertEquals(six, 6)
+    }
+
+    @Test
+    fun pushesTwoPopsTwoElements() {
+        stack.push(5)
+        stack.push(7)
+
+        val seven = stack.pop()
+        val five = stack.pop()
+
+        assertEquals(seven, 7)
+        assertEquals(five, 5)
+
+    }
+    @Test
+    fun pushesFourElements() {
+        stack.push(5)
+        stack.push(7)
+        stack.push(8)
+        stack.push(7)
+
+        assertEquals(stack.size(), 4)
+    }
+
 }
