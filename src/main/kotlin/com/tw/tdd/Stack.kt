c9a529859ffcdf4e88b863cb3fb14c43a7071d11
Gaspar Soto <gsoto@thoughtworks.com>
2019-02-22 12:56:43 -0300

    adds methods: push - pop - size

--- src/main/kotlin/com/tw/tdd/Stack.kt
+++ src/main/kotlin/com/tw/tdd/Stack.kt
@@ -1,5 +1,18 @@
 package com.tw.tdd
 
 class Stack {
+    private val data = IntArray(2)
+    private var index : Int = 0
 
+    fun push(element: Int) {
+        data[index++] = element
+    }
+
+    fun size(): Int {
+        return index
+    }
+
+    fun pop(): Int {
+        return data[--index]
+    }
 }
--- src/test/kotlin/com/tw/tdd/StackTest.kt
+++ src/test/kotlin/com/tw/tdd/StackTest.kt
@@ -9,4 +9,46 @@ class StackTest {
     fun firstTest() {
         assertEquals(1,1)
     }
+
+    @Test
+    fun pushOneElement() {
+        val stack = Stack()
+
+        stack.push(6)
+
+        assertEquals(stack.size(), 1)
+    }
+
+    @Test
+    fun push2Elements() {
+        val stack = Stack()
+
+        stack.push(6)
+        stack.push(6)
+
+        assertEquals(stack.size(), 2)
+    }
+
+    @Test
+    fun popElement() {
+        val stack = Stack()
+        stack.push(6)
+
+        val result = stack.pop()
+
+        assertEquals(result, 6)
+    }
+
+    @Test
+    fun popTwoElements() {
+        val stack = Stack()
+        stack.push(6)
+        stack.push(9)
+
+        val firstResult = stack.pop()
+        val secondResult = stack.pop()
+
+        assertEquals(firstResult, 9)
+        assertEquals(secondResult, 6)
+    }
 }
